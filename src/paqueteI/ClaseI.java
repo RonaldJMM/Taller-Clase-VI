920364ef5782499d4b314db677914176c41b1bf2
Ronald Jefrey Moreno Mora <morenoronald7@gmail.com>
2018-02-26 14:17:36 -0500

    Carga de Archivos del Taller
    Carga de los archivos del taller de la clase VI

---
+++ src/paqueteI/ClaseI.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteI;
+
+/**
+ *
+ * @author Ronald Jefrey Moreno Mora - 301 - Programacion II
+ */
+public class ClaseI {
+    //Variable de las clases.
+    public int variable1;
+    public String variable2;
+    public double variable3;
+    public float variable4;
+    public double variable5;
+    
+    /**
+     * Metodo publico de tipo entero.
+     * @return variable entera.
+     */
+    public int metodo1(){
+        return variable1;
+       
+    }
+    /**
+     * Metodo publico de tipo String.
+     * @return variable tipo String.
+     */
+    public String metodo2(){
+        return variable2;
+    }
+    /**
+     * Metodo publico de tipo vacio.
+     *
+     */
+    public void metodo3(){
+        
+    }
+    /**
+     * Metodo publico de tipo flotante.
+     * @return variable de tipo flotante.
+     */
+    public float metodo4(){
+        return variable4;
+    }
+    
+    /**
+     * Metodo publico de tipo double.
+     * @return variable de tipo double.
+     */
+    public double metodo5(){
+        return variable5;
+    }
+    
+    @Override
+    public String toString(){
+        return "Datos del objeto 1: int= "+variable1+" String= "+variable2+" double="+variable3+" float="+variable4+" double="+variable5;
+    };
+}
---
+++ src/principal/Main.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Importacion de los paquetes y las clases.
+ */
+package principal;
+import paqueteI.ClaseI;
+import paqueteII.ClaseII;
+import paqueteIII.ClaseIII;
+/**
+ *
+ * @author Ronald Jefrey Moreno Mora - 301 - Programacion II
+ */
+public class Main {
+
+    /**
+     * metodo de la clase Main.
+     */
+    public static void main(String[] args) {
+        //objetos de cada una de las clases.
+       ClaseI objeto1 = new ClaseI();
+       ClaseII objeto2 = new ClaseII();
+       ClaseIII objeto3 = new ClaseIII();
+       
+       //Impresion de cada uno de los atributos de cada objeto de las clases correspondientes.
+       System.out.println("Objeto 1 Atributo int: "+objeto1.variable1);
+       System.out.println("Objeto 1 Atributo String: "+objeto1.variable2);
+       System.out.println("Objeto 1 Atributo double: "+objeto1.variable3);
+       System.out.println("Objeto 1 Atributo float: "+objeto1.variable4);
+       System.out.println("Objeto 1 Atributo double: "+objeto1.variable5);
+       System.out.println("Objeto 2 Atributo int: "+objeto2.variable1);
+       System.out.println("Objeto 2 Atributo String: "+objeto2.variable2);
+       System.out.println("Objeto 2 Atributo double: "+objeto2.variable3);
+       System.out.println("Objeto 2 Atributo float: "+objeto2.variable4);
+       System.out.println("Objeto 2 Atributo double: "+objeto2.variable5);
+       System.out.println("Objeto 3 Atributo int: "+objeto3.variable1);
+       System.out.println("Objeto 3 Atributo String: "+objeto3.variable2);
+       System.out.println("Objeto 3 Atributo double: "+objeto3.variable3);
+       System.out.println("Objeto 3 Atributo float: "+objeto3.variable4);
+       System.out.println("Objeto 3 Atributo double: "+objeto3.variable5);
+       
+       //Cambio de dato en las variables de cada objeto de las clases.
+       objeto1.variable1=20;
+       objeto1.variable2="Veinte";
+       objeto1.variable3=32.3;
+       objeto1.variable4=70.8F;
+       objeto1.variable5=20.7;
+       objeto2.variable1=10;
+       objeto2.variable2="Quince";
+       objeto2.variable3=56.45;
+       objeto2.variable4=29.4F;
+       objeto2.variable5=334.4;
+       objeto3.variable1=56;
+       objeto3.variable2="Cincuenta y Cinco";
+       objeto3.variable3=56.657;
+       objeto3.variable4=23.456F;
+       objeto3.variable5=62.74;
+       
+       //Llamado de cada uno de los metodos de los objetos de las clases.
+       objeto1.metodo1();
+       objeto1.metodo2();
+       objeto1.metodo3();
+       objeto1.metodo4();
+       objeto1.metodo5();
+       objeto2.metodo1();
+       objeto2.metodo2();
+       objeto2.metodo3();
+       objeto2.metodo4();
+       objeto2.metodo5();
+       objeto3.metodo1();
+       objeto3.metodo2();
+       objeto3.metodo3();
+       objeto3.metodo4();
+       objeto3.metodo5();
+       
+       //Llamado del metodo toString de cada objeto de sus clases.
+       System.out.println(objeto1.toString());
+       System.out.println(objeto2.toString());
+       System.out.println(objeto3.toString());
+    }
+    
+}
